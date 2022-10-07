65196dbe0d3b07ebe057ed0900f55976c369e7bd
franklinaldo filho <frank@junior.localdomain>
2022-10-07 16:48:57 -0300

    gerando usuario via seed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = \App\Models\User::factory()->create();
+
+        echo "\033[26m". "Dados para autenticação do usuário \n";
+        echo "\033[32m"."Email: ". $user->email ."\n";
+        echo "\033[32m"."Senha: ". "password" ."\n";
+    }
+}
